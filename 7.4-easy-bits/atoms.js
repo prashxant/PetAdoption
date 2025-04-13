4d952c1887e5101e635191cdfc82ad88367e5917
prashxant <notxdead0@gmail.com>
2025-04-13 18:46:33 +0530

    linkdin bar

---
+++ 7.4-easy-bits/atoms.js
@@ -0,0 +1,24 @@
+import { atom } from "recoil";
+
+
+
+
+export const networkAtom = atom({
+    key:"networkAtom",
+    default: 104,
+}) 
+
+export const jobsAtom = atom({
+    key:"jobsAtom",
+    default: 33,
+}) 
+
+export const notificationAtom = atom({
+    key:"notificationAtom",
+    default: 12,
+}) 
+
+export const messagingAtom = atom({
+    key:"messagingAtom",
+    default: 4328,
+}) 
--- 7.4-easy-bits/src/App.jsx
+++ 7.4-easy-bits/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { RecoilRoot, useRecoilValue } from 'recoil'
+import { jobsAtom, messagingAtom, networkAtom, notificationAtom } from '../atoms'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  return <RecoilRoot>
+    <MainApp/>
+    </RecoilRoot>
+ 
+  
+}
+
+
+function MainApp(){
+  const networkNotificationCount = useRecoilValue(networkAtom)
+  const jobsCount = useRecoilValue(jobsAtom)
+  const messageCount = useRecoilValue(messagingAtom)
+  const notificationCount = useRecoilValue(notificationAtom)
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+        <button>home</button>
+        <button> my network ({networkNotificationCount>=100? "99+" : networkNotificationCount})</button>
+        <button>Job({jobsCount>=100? "99+" : jobsCount})</button>
+        <button>Message({messageCount>=100? "99+" : messageCount})</button>
+        <button>Notifcation({notificationCount>=100? "99+" : notificationCount})</button>
+        <button>You</button>
+      
+    </div>)
 }
+
 
 export default App
